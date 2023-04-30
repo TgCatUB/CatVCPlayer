0b8ce4c6f014091b68092711e8eb8fc44608df0d
Jisan09 <66010486+Jisan09@users.noreply.github.com>
2023-04-30 18:11:01 +0000

    pylint: auto fixes

--- inlinevc.py
+++ inlinevc.py
@@ -89,7 +89,9 @@ async def skipvc(event):
 
 @catub.tgbot.on(CallbackQuery(data=re.compile(r"^repeatvc")))
 async def repeatvc(event):
-    buttons = [[Button.inline(k.text, data=k.data[2:1]) for k in i] for i in event.buttons]
+    buttons = [
+        [Button.inline(k.text, data=k.data[2:1]) for k in i] for i in event.buttons
+    ]
     if vc_player.PLAYING:
         song_input = vc_player.PLAYING["path"]
         stream = vc_player.PLAYING["stream"]
