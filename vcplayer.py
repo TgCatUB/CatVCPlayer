e129c5f6cb43359849d920594c073f8b2269a109
Zarox <85471432+MineisZarox@users.noreply.github.com>
2023-05-02 16:30:42 +0530

    bug fix

--- vcplayer.py
+++ vcplayer.py
@@ -220,10 +220,10 @@ async def play_video(event):
     reply = await event.get_reply_message()
     if av_check:
         av_options = ["Video", Stream.video]
-        reply_media = reply.video
+        reply_media = reply.video 
     else:
         av_options = ["Audio", Stream.audio]
-        reply_media = reply.media
+        reply_media = reply.media if reply else False
 
     if "-f" in input_str:
         input_str = input_str.replace("-f", "").strip()
